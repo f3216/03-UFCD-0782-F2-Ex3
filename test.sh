ea17cf363a4e95a35e95575fb7a9b6ffb788b00a
f3216 <122741727+f3216@users.noreply.github.com>
2023-01-29 20:16:59 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -10,9 +10,11 @@ output_line_2=$(echo "$program_output" | head -n 2 | tail -n 1)
 
 if [ "$output_line_1" == "$expected_output_1" ] && [ "$output_line_2" == "$expected_output_2" ] ] ; then
   echo "Aprovado: A saída é correcta"
+  echo "::set-env name=TEST_RESULT::Aprovado"
 else
   echo "Esperada a saída '$expected_output_1' mas o programa devolveu: $output_line_1"
   echo "Esperada a saída '$expected_output_2' mas o programa devolveu: $output_line_2"
+  echo "::set-env name=TEST_RESULT::Falha"
   exit 1
 fi
 
